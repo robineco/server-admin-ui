94e08a6a2650c62f05e74d812fc8a54bd95a4755
robineco <robin.schlund@tutanota.de>
2021-02-02 14:26:02 +0100

    fixed Dockerfile

--- de.robinscloud.admin-ui/.eslintrc.js
+++ de.robinscloud.admin-ui/.eslintrc.js
@@ -3,15 +3,12 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
+  extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/typescript/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
+    'linebreak-style': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
